1784c8272076ac627d3f875d7e8dca93d6750a2a
Rustam Z. Khaliullin <rustam@khaliullin.com>
2019-02-28 23:23:49 -0500

    3x3 works, larger boards need better interface and are not tested

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,62 +1,90 @@
 #!/Users/rustam/anaconda/bin/python
 import sys 
+import math
+
+# This global variable keeps track of recursion depth
+Depth = 0
 
 # Library
 
+# This recursive function updates one position in the Board
+# and returns the best outcome for Symbol player
 def BestMove( Board, Symbol ):
 # Now the algorithm is quite dumb. It works only if it can reach the 
 # bottom of the tree. To make it smarter we need to estimate how many 
 # branches lead to win from the current state.
 
+ global Depth
  BestOutcome = 0 # loss(0), tie(1), win(2)
+ BestPosition = -1 # this is a local variable to keep current best move
+ TrialOutcome = -1
 
- ScratchBoard=[]
- for TrialPosition in range(0, BoardSize2):
-  ScratchBoard.append(Board[TrialPosition])
+ ScratchBoard=Board.copy()
+ for TrialPosition in range(BoardSize2):
 
- # these are locals, not returned
- BestPosition = -1
- for TrialPosition in range(0, BoardSize2):
+  # return the scratch board to its original state 
+  # that is, the state before the last trial move
+  ScratchBoard=Board.copy()
+
   if ScratchBoard[TrialPosition] == 0:
 
    ScratchBoard[TrialPosition] = Symbol
-
    myState=WLT( ScratchBoard, BoardSize )
 
    if (myState == Symbol): # Symbol wins!
     BestPosition = TrialPosition
     BestOutcome = 2
     break # do not consider any other (possibly winning) moves
+   elif (myState == 3 and BestOutcome == 0): # It's a tie
+    BestPosition = TrialPosition
+    BestOutcome = 1
+    continue
 
    # let the opponent choose its best move
+   Depth += 1
    BestMove ( ScratchBoard, InvertSymbol(Symbol) )
    myState=WLT( ScratchBoard, BoardSize )
+   Depth -= 1
 
    if (myState == InvertSymbol(Symbol) ): # Inverted symbol (opponent) wins!
     # reject this move simply by going to the next TrialPosition
     # so that the next BestMove is never called and 
     # this branch in the decision tree is terminated
     continue
+   elif (myState == 3 and BestOutcome == 0): # It's a tie
+    BestPosition = TrialPosition
+    BestOutcome = 1
+    continue
 
    # Step deeper in the recursive tree.
    # This is where the magic happens: 
    # if this BestMove returns a better outcome than the 
    # current best we accept the 
+   Depth += 1
    TrialOutcome = BestMove ( ScratchBoard, Symbol )
+   Depth -= 1
    if TrialOutcome > BestOutcome:
-    BestOutcome = TrialOutcome
     BestPosition = TrialPosition
+    BestOutcome = TrialOutcome
 
- if BestPosition > -1:
+  #if (Depth==0):
+  # print( (Depth+1)*"!", Symbol, TrialPosition, BestPosition, BestOutcome, ScratchBoard)
+
+ # done with the main trial loop
+
+ if BestOutcome > 0:
   Board[BestPosition] = Symbol
  else:
-  # This handles a "no-win" situation:
+  # This handles an "impossible-to-win" situation:
   # We make a move into the first empty cell
-  # In the hope that the opponent does not make
-  # it BEST move
+  # in hope that the opponent does not make their best move
   for TrialPosition in range(0, BoardSize2):
    if Board[TrialPosition] == 0:
     Board[TrialPosition] = Symbol
+    break
+
+ #if (Depth==0):
+ # print( (Depth+1)*">", Symbol, BestOutcome, BestPosition, Board)
 
  return BestOutcome
 
@@ -119,7 +147,6 @@ def WLT( Board, BoardSize ):
 
     return 3 # return 3 means tie
 
-
 def InvertSymbol( Symbol ):
  if Symbol==1:
   return 2
@@ -131,7 +158,11 @@ def InvertSymbol( Symbol ):
 def GetInput( Board, Symbol ):
 
  Accepted=False
- Prompt = "Enter position [0-%d]: " % BoardSize2 
+ if Symbol == 1:
+  CharSymbol = "X"
+ else:
+  CharSymbol = "Q"
+ Prompt = "Enter position for %1s [0-%d]: " % (CharSymbol,BoardSize2-1)
 
  while not Accepted:
   
@@ -146,31 +177,52 @@ def GetInput( Board, Symbol ):
 
 
 # Display the board
-def drawBoard( Board, BoardSize ):
-  for row in range(BoardSize):
-    for column in range(BoardSize):
-      print(Board[BoardSize * row + column], end =" ")
-    print("\n ")
+def drawBoard( Board ):
+ BoardSize = int(math.sqrt(len(Board)))
+ for row in range(BoardSize):
+  for column in range(BoardSize):
+   iBoard = Board[BoardSize * row + column]
+   if iBoard == 0:
+    Symbol = BoardSize * row + column
+   elif iBoard == 1:
+    Symbol = "X"
+   else:
+    Symbol = "Q"
+   print(Symbol, end ="")
+  print("")
 
 ### THe BEGINNING OF THE MAIN FUNCTION!!
 
 BoardSize=3
 BoardSize2=BoardSize*BoardSize
 
-OurTurn = 0 # random 0 or 1
+OurTurn = 1 # random 0 or 1
 OurSymbol = 1 # random: 1 or 2
 OppSymbol = InvertSymbol(OurSymbol)
 
-# how to best represent the board? X -> 1, O -> 2, empty cell -> 0
+# how to best represent the board? X -> 1, Q -> 2, empty cell -> 0
 Board = []
 for element_in_board in range(0, BoardSize2):
  Board.append(0)
+
+# create initial Board
+#Board[0]=1
+#Board[6]=1
+#Board[2]=2
+#Board[3]=2
+#Board[4]=2
+#OurTurn = 1
+
+# Draw empty board
+if not OurTurn:
+ drawBoard ( Board )
 
 # start the main loop
 while True:
 
  # Let user to decide the borad size and draw the board
  if OurTurn:
+  print("Here is my move:")
   BestMove( Board, OurSymbol )
   OurTurn = False
  else:
@@ -178,7 +230,7 @@ while True:
   OurTurn = True
 
  State = WLT( Board, BoardSize )
- drawBoard ( Board, BoardSize )
+ drawBoard ( Board )
 
  if State > 0 :
   break
@@ -188,7 +240,7 @@ while True:
 if State==1:
  print("X wins!")
 elif State==2:
- print("O wins!")
+ print("Q wins!")
 else:
- print("Tie")
+ print("How unfortunate, it's a tie!")
 
